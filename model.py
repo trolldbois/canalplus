38e8927059892a434b1969b4e272a8ad7fdefdb4
Loic Jaquemet <loic.jaquemet+github@gmail.com>
2011-02-25 22:17:34 -0700

    cleaning comments and api

--- model.py
+++ model.py
@@ -5,35 +5,20 @@
 #
 
 import logging,os,urlparse,re
-
-
 from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey, UniqueConstraint, DateTime
 from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+#local exception
+from exception import Wtf
 
 log=logging.getLogger('model')
-
-from sqlalchemy.ext.declarative import declarative_base
-
 Base = declarative_base()
-
-
-
-
-class Wtf(Exception):
-  def __init__(self,obj=None):
-    self.obj=obj
-  def __str__(self):
-    if self.obj is not None:
-      return '%s'%(self.obj.__dict__)
-    return '' 
-
-
-
 
 
 class Theme(Base):
   '''
-    Emission are grouped into categories, which are part off one of the 6 main Themes.
+    Theme is the top-most classification.
+    Themes - > Categories -> Emissions
   '''
   __tablename__="themes"
   
@@ -49,17 +34,15 @@ class Theme(Base):
     self.url=url
     self.text=text
     return
-  
-  log=log
-  data=None
-  root=None
-
-  #
   def __repr__(self):
     return '<Theme %s: %s>'%(self.tid, repr(self.text))  
 
 
 class Categorie(Base):
+  '''
+    Categorie regroups several emissions.
+    Themes - > Categories -> Emissions
+  '''
   __tablename__="categories"
   
   cid=Column('rowid',Integer, primary_key=True)
@@ -68,28 +51,19 @@ class Categorie(Base):
   emissions=relationship("Emission",backref='categorie')
   
   __table_args__= (UniqueConstraint(text, 'desc') ,{}) 
-
-  log=log
-  #catPath='/html/body/div[2]/div[9]/div[3]/div/h3'
-  aPath='./a[1]'
-
   def __init__(self,cid=None,text=None,tid=None):
     self.cid=cid
     self.text=text
     self.tid=tid
     return
-
-  #
   def __repr__(self):
     return '<Categorie %s-%s: %s>'%(self.tid,self.cid,repr(self.text))  
   
 
 class Emission(Base):
   '''
-    An emission is described on a dynamice page.
-    It is accessible by URL link self.url
-    It an be saved to file.
-    It's content is accessible by memory access to self.text
+    An emission is described on the main page.
+    An more complete description of it's video contents is accessible by URL link self.url
   '''
   __tablename__="emissions"
 
@@ -102,34 +76,28 @@ class Emission(Base):
 
   __table_args__= (UniqueConstraint(url, 'url'), UniqueConstraint(text, 'desc') ,{}) 
 
-  log=log
-  aPath='.'
-  pidRE='.+pid(\d+).+'
-  # the regexp to get an Video Id from a url
-  vidRE='.vid=(\d+)'
-
   def __init__(self,pid=None,cid=None,url=None,text=None):
     self.pid=pid
     self.cid=cid
     self.url=url
     self.text=text
     return
-    
-  #
   def __repr__(self):
     return '<Emission %s pid="%s">'%(repr(self.text),self.pid)  
 
 
 class Video(Base):
   '''
-    Une video est determinee par une url REST self.srcUrl + self.vid
+    Videos are referenced on a Emission page.
+    Several Videos can be found, with quick time expiration ( by day)
+    Each video has a more complete XML description which can be found at self.url= self.srcUrl + self.vid
+    This XML data often references all Videos & streams on the Emission page.
   '''
   __tablename__="videos"
   vid=Column(Integer,primary_key=True)
   pid=Column(Integer,ForeignKey('emissions.pid'))
   text=Column("desc",String(1000))
   url=Column(String(1000))
-  #self.srcUrl%(self.vid)
   streams=relationship("Stream",backref='video')
 
   __table_args__= (UniqueConstraint(url, 'url'), UniqueConstraint(text, 'desc') ,{}) 
@@ -145,9 +113,6 @@ class Video(Base):
     self.parsed=False
     return
 
-  def getUrl(self):
-    return self.url
-  #
   def update(self,title,subtitle):
     self.title=title
     self.subtitle=subtitle
@@ -155,15 +120,7 @@ class Video(Base):
     self.parsed=True
     log.debug('Updated %s'%self)    
     return
-  #
-  def isParsed(self):
-    return self.parsed
-  #
-  def getId(self):
-    ''' gets a video ID    '''
-    return self.vid
-    
-  #  
+
   def bestStream(self):
     if len(self.streams) == 0:
       return None
@@ -172,18 +129,20 @@ class Video(Base):
         if qual in self.streams:
           return self.streams[qual]
       return self.streams[0]
-  #
+
   def __repr__(self):
     return "<Video %d-%d: %s>"%(self.pid, self.vid,repr(self.text))
 
 
 class Stream(Base):
-  __tablename__="streams"
+  '''
+    Streams are the link to video content.
+    Several links with different qulity can be found for one video.
+  '''
 
-  #id=Column(Integer,primary_key=True)
+  __tablename__="streams"
   vid=Column(Integer,ForeignKey('videos.vid'),primary_key=True)
   quality=Column(String(50),primary_key=True)
-  #url=Column(String(1000), UniqueConstraint('urlu'))
   url=Column(String(1000))
 
   __table_args__  = (UniqueConstraint(url, 'url'), {}) 
@@ -194,8 +153,8 @@ class Stream(Base):
     self.url=urlparse.urlparse(url).geturl()
     if self.url is None:
       raise Wtf()
+    return  
   
-  #
   def fetchStream(self):
     ''' on recupere le stream avec un outils externe ( rtmpdump )
     '''
@@ -207,8 +166,7 @@ class Stream(Base):
   def makeFilename(self):
     self.filename=os.path.sep.join(['./output',os.path.basename(self.url)])
     return self.filename
-    
-  
+ 
   def __repr__(self):
     low=min(len(self.url),20)
     up=max(0,len(self.url)-20)    
